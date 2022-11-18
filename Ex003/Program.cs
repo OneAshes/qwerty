88915c268b5d4a128944bde337a76fe771012395
OneAshes <118503024+OneAshes@users.noreply.github.com>
2022-11-18 14:21:30 +0800

    ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2463142 Hello World!");
+        }
+    }
+}
